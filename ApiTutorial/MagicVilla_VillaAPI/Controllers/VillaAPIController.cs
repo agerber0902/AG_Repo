dc1f363c81e77820480370a2062716aca6fafa6d
agerber0902 <agerber0902@gmail.com>
2024-01-31 10:28:09 -0500

    Immplement Swagger

--- ApiTutorial/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ ApiTutorial/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,10 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using MagicVilla_VillaAPI.Models;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+    [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPICoontroller : ControllerBase
     {
+
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>{
+                new Villa{Id = 1, Name = "Pool View"},
+                new Villa{Id = 2, Name = "Beach View"}
+            };
+        }
 
     }
 
